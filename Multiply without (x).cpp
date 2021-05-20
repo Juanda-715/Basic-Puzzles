66cb3a0cebed48c8ea911693f1a4a0cdd3022225
Juanda-715 <84536983+Juanda-715@users.noreply.github.com>
2021-05-20 13:05:08 -0500

    Multiplying using additions.
    In this excersise you will find how to multiply without using (*) in cpp.

---
+++ Multiply without (x).cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+double multiplicar(double, double);
+int main()
+{
+	double a, b, res;
+	cout << "Ingrese dos numeros para multiplicar: " << endl;
+	cout << "1: ";
+	cin >> a;
+	cout << "2: ";
+	cin >> b;
+	res = multiplicar(a, b);
+	cout << "El restultado de la multiplicacion entre " << a << " y " << b << " es: " << res << endl;
+}
+double multiplicar(double a, double b)
+{
+	double resultado = 0, z;
+	if (a < 0)
+	{
+		z = a;
+		a = b;
+		b = z;
+	}
+	if (a < 0 && b < 0)
+	{
+		a = a - (a + a);
+		b = b - (b + b);
+		for (int c = 1; c <= a; c++)
+		{
+			resultado += b;
+		}
+	}
+	else
+	{
+		for (int c = 1; c <= a; c++)
+		{
+			resultado += b;
+		}
+	}
+
+		return resultado;
+}
